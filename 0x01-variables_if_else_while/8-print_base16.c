50a0956b5e01efdd19ab7f855dcb640c3a395f9f
S3RG1O1994 <se.diaz60@gmail.com>
2020-02-06 23:09:44 +0000

    Answers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int x;
+	int y;
+
+	for(y = '0'; y <= '9'; y++)
+	{
+		putchar(y);
+	}
+	for (x = 'a'; x <= 'f'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
