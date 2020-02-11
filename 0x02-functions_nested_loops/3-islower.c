c9f2e0851059966105ddaf89c2e28787bfe81da9
S3RG1O1994 <se.diaz60@gmail.com>
2020-02-11 17:32:50 +0000

    Corrections-3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _islower - check the code for Holberton School students.
+ * @c: Variable initial of the function.
+ * Return: print in number if character is lowercase or not.
+ */
+int _islower(int c)
+{
+
+	if (c >= 'a' || c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
