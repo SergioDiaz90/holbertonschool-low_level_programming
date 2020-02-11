fe21ded84a02f4e795b6157f862740b42b928f6d
S3RG1O1994 <se.diaz60@gmail.com>
2020-02-11 18:46:29 +0000

    proof

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * _abs - check the code for Holberton School students.
+ * @x:  variable initial of the function.
+ * Return: print values absolute.
+ */
+int _abs(int x)
+{
+	_abs(x);
+	return (0);
+}
