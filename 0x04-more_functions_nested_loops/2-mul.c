322627743dcff93bc561ee344be8a3f885b0fc58
S3RG1O1994 <se.diaz60@gmail.com>
2020-02-13 18:57:53 +0000

    Answers-2 with update of code

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * mul - multiply two integers..
+ * @a:  variable initial of the function.
+ * @b:  variable initial of the function.
+ * Return: Outcome of multiplies two integers..
+ */
+int mul(int a, int b)
+{
+	int x = a * b;
+
+	return (x);
+}
