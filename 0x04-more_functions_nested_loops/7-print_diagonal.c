0a6d65e671084b457ffb872ca65983bb5181d3ea
S3RG1O1994 <se.diaz60@gmail.com>
2020-02-13 22:21:19 +0000

    Answers-7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagonal - print numbers from 0 to 9.
+ * @n: variable initial of the function.
+ * Return: col of numbers 0 to 9 followed by a new line.
+ */
+void print_diagonal(int n)
+{
+	int x;
+	int a;
+
+	if (n > 0)
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (a = 0; a < x; a++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
