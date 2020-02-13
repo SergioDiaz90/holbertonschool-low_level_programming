8c1cd8c43173969a8d653545e11ca8da749be156
S3RG1O1994 <se.diaz60@gmail.com>
2020-02-13 20:17:12 +0000

    Corrections

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,8 +2,6 @@
 #include <stdio.h>
 /**
  * more_numbers - print numbers from 0 to 9.
- * i: variable initial of the loop.
- * line: varible second of the loop.
  * Return: col of numbers 0 to 9 except 2 and 4 followed by a new line.
  */
 void more_numbers(void)
