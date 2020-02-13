d362c333e9ab3d2fd47de2593e659fc3536ec090
S3RG1O1994 <se.diaz60@gmail.com>
2020-02-13 20:13:01 +0000

    Answers-5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * more_numbers - print numbers from 0 to 9.
+ * i: variable initial of the loop.
+ * line: varible second of the loop.
+ * Return: col of numbers 0 to 9 except 2 and 4 followed by a new line.
+ */
+void more_numbers(void)
+{
+	int i;
+	int line;
+
+	for (line = 0; line <= 10; line++)
+	{
+		for (i = 0 ; i < 15 ; i++)
+		{
+			if (i > 9)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar ((i % 10) + '0');
+		}
+	_putchar ('\n');
+	}
+}
