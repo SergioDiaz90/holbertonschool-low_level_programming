76ea2f79428dc22bc54ddc0da5b8e197ec312de7
S3RG1O1994 <se.diaz60@gmail.com>
2020-02-14 03:44:44 +0000

    New Corrections

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,22 +13,22 @@ int main(void)
 		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
-			putchar(' ');
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
-			putchar(' ');
 		}
 		else if (i % 3 == 0)
 		{
 			printf("Fizz");
-			putchar(' ');
 		}
 		else
 		{
 			printf("%d", i);
-			putchar(' ');
+		}
+		if (i != 100)
+		{
+			printf(" ");
 		}
 	}
 	putchar('\n');
