c432c0c4487f258310a85ac6baec7992ba999dc7
S3RG1O1994 <se.diaz60@gmail.com>
2020-02-18 23:28:10 +0000

    Corrections-keys

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,19 +15,13 @@ void puts_half(char *str)
 	}
 
 	if (i % 2 != 0)
-	{
 		x = (i - 1) / 2;
-	}
 
 	else
-	{
 		x = i / 2;
-	}
 
 	for (x = x; x < i ; x++)
-	{
 		_putchar (*(str + x));
-	}
 	_putchar('\n');
 
 }
