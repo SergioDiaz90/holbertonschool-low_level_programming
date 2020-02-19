b8734157238b73567845dc31cecb8706ee7defd3
S3RG1O1994 <se.diaz60@gmail.com>
2020-02-19 23:39:23 +0000

    Answers-9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strcpy - check the code
+ *
+ * @dest : Pointer that is parameter in the function.
+ * @src : String 1
+ * Return: Always dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' ; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
