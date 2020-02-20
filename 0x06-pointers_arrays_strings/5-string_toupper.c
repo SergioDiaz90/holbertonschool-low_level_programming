a0e52c3cd0899da2f5d3a2e661466a053f85bf2f
S3RG1O1994 <se.diaz60@gmail.com>
2020-02-20 22:44:41 +0000

    Corrections in conditional for

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,14 +3,14 @@
 /**
  * *string_toupper - plus two strings
  * @i : pointer
- * Return: Always dest.
+ * Return: Always i.
  */
 char *string_toupper(char *i)
 {
 	int count;
 	int aux;
 
-	for (count = 0; count < i; count++)
+	for (count = 0; i[count] != '\0'; count++)
 	{
 		if (i[count] < 96 && i[count] > 123)
 		{
