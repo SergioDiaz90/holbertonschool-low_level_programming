7a21db5d7909d1baff9971fb7e0b16e99515e7f4
S3RG1O1994 <se.diaz60@gmail.com>
2020-02-20 22:02:00 +0000

    Corrections

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * reverse_array - plus two strings
+ *
+ * @a : Pointer that is parameter in the function.
+ * @n : String 1
+ * Return: Always dest.
+ */
+void reverse_array(int *a, int n)
+{
+	int rev;
+	int aux;
+
+	for (rev = 0; rev < n; rev++)
+	{
+		aux = a[rev];
+		a[rev] = a[n - 1];
+		a[n--] = aux;
+	}
+}
