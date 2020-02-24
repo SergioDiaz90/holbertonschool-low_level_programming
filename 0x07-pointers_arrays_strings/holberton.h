67591e9adbb10ee67e2b036b55199cef103ab83c
S3RG1O1994 <se.diaz60@gmail.com>
2020-02-24 16:57:20 +0000

    Second answers

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -8,5 +8,6 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
