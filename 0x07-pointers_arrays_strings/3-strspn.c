cd57b7556154f9653f22fa6aa25d655cf3b62e33
S3RG1O1994 <se.diaz60@gmail.com>
2020-02-24 19:59:32 +0000

    Corrections

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,7 +12,7 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int aux = 0;
 	int cnt2;
 
-	for (cnt = 0; s[cnt] != 32; cnt++)
+	for (cnt = 0; s[cnt] != 32 && s[cnt] != '\0'; cnt++)
 	{
 		for (cnt2 = 0; accept[cnt2] != '\0'; cnt2++)
 		{
