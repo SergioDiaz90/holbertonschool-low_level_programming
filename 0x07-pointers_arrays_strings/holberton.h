a9b33155d47f58fe8420bd74069652005d7e6099
S3RG1O1994 <se.diaz60@gmail.com>
2020-02-24 16:07:54 +0000

    First-Answers

---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
