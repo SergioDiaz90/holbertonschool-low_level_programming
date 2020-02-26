728b9970c5790a745d87438e2e828f9e9fe4be51
S3RG1O1994 <se.diaz60@gmail.com>
2020-02-26 17:54:17 +0000

    Correction-styles-betty

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
- * *_strlen_recursion - find character in c
+ *factorial - find character in c
  *
- * @s : Pointer that is parameter in the function.
+ * @n : Variable that is parameter in the function.
  * Return: Always pointer.
  */
 int factorial(int n)
@@ -10,14 +10,14 @@ int factorial(int n)
 
 	if (n < 0)
 	{
-		return(-1);
+		return (-1);
 	}
-	else if(n == 0)
+	else if (n == 0)
 	{
 		return (1);
 	}
 	else
 	{
-		return(n * factorial(n - 1));
+		return (n * factorial(n - 1));
 	}
 }
