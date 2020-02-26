656e3a6f61b5587aab2bcf08beae52f059dcfae7
S3RG1O1994 <se.diaz60@gmail.com>
2020-02-26 15:54:31 +0000

    Answers-1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * *_print_rev_recursion - find character in c
+ *
+ * @s : Pointer that is parameter in the function.
+ * Return: Always pointer.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_puts_recursion(s + 1);
+	}
+	_putchar(*s);
+}
