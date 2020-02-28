13cef7ac54ec88cb61cbde01e230ef58a0d42e26
S3RG1O1994 <se.diaz60@gmail.com>
2020-02-28 23:32:24 +0000

    Answers 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function general.
+ *
+ * @argc : Number of arguments for the function.
+ * @argv : Array unidimentional of strings
+ * Return: Always pointer.
+ */
+
+int main(int argc  __attribute__((unused)),char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This project is for learn use of arguments for function main ARGC and ARGV
