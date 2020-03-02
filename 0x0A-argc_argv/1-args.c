d89b7dca8b5f454b5776854817c143d065ba6e06
Sergio_Diaz <39101811+S3RG1O1994@users.noreply.github.com>
2020-03-01 23:20:22 -0500

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,20 +10,6 @@
 
 int main(int argc, char **argv __attribute__((unused)))
 {
-	int count;
-
-	if (argc > 1)
-	{
-		for (count = 1; count < argc; count++)
-		{
-		}
-
-		printf("%d\n", count);
-	}
-	else
-	{
-		printf("%d\n", 0);
-	}
-
+	printf("%i\n", argc - 1);
 	return (0);
 }
