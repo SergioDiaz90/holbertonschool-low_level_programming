9dc12a7cbca95d11576b79667ef437b4dc9c310a
Sergio_Diaz <39101811+S3RG1O1994@users.noreply.github.com>
2020-03-01 23:06:27 -0500

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-# include  < stdlib.h >
+#include <stdlib.h>
 /**
  * main - function general.
  *
@@ -8,7 +8,7 @@
  * Return: Always pointer.
  */
 
-int main(int argc  __attribute__((unused)),char *argv[])
+int main(int argc  __attribute__((unused)),char **argv)
 {
 	printf("%s\n", argv[0]);
 	return (0);
