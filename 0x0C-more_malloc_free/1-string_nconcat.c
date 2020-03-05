33659bbd033d628f0b1b639d3933347124fc5b22
fernando <se.diaz60@gmail.com>
2020-03-05 20:05:38 +0000

    Corrections-conditions

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,38 +17,37 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *aux = NULL;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
+
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-	if (n != '\0')
+
+	for (cnt = 0; s1[cnt] != '\0'; cnt++)
+		;
+	for (cnt2 = 0; cnt2 < n ; cnt2++)
+		;
+	if (n > cnt2)
+		n = cnt2;
+
+	lenght = (cnt + 1) + cnt2;
+	aux = malloc(sizeof(char) * lenght);
+	cnt2 = 0;
+
+	if (aux == NULL)
+		return (NULL);
+
+	for (cnt2 = 0; cnt2 < (lenght - 1); cnt2++)
 	{
-		for (cnt = 0; s1[cnt] != '\0'; cnt++)
-		{}
-		for (cnt2 = 0; cnt2 < n ; cnt2++)
-		{}
-
-		lenght = (cnt + 1) + cnt2;
-
-		aux = malloc(sizeof(char) * lenght);
-		cnt2 = 0;
-
-		for (cnt2 = 0; cnt2 < (lenght - 1); cnt2++)
+		if ((s1[cnt2] != '\0') && (cnt2 < cnt))
 		{
-			if ((s1[cnt2] != '\0') && (cnt2 < cnt))
-			{
-				aux[cnt2] = s1[cnt2];
-			}
-			else
-			{
-				aux[cnt2] = s2[cnt3];
-				cnt3++;
-			}
+			aux[cnt2] = s1[cnt2];
 		}
-		aux[cnt2] = '\0';
+		else
+		{
+			aux[cnt2] = s2[cnt3];
+			cnt3++;
+		}
 	}
+	aux[cnt2] = '\0';
 	return (aux);
 }
