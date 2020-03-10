d08421ae7d690f5a4acb085a5f5f9f8ea7b870e0
S3RG1O1994 <se.diaz60@gmail.com>
2020-03-10 03:12:59 +0000

    Answers-3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,14 +7,14 @@
  *@owner: the owner
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 
 	float age;
 
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
