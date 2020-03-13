bee49ac377c326e0f0652de31d14bfd63ab63d06
S3RG1O1994 <se.diaz60@gmail.com>
2020-03-13 15:00:58 +0000

    Init-project

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+/**
+ * *sum_them_all - plus of parameters n.
+ * @n: number of arguments.
+ *
+ * Return: result plus all arguments.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list date;
+	unsigned int i, sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(date, n);
+
+	for (i = 0; i < n; i++)
+		sum = sum + va_arg(date, unsigned int);
+
+	va_end(date);
+	return (sum);
+}
