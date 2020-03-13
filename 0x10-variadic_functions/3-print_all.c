c0835998a0faf507deb0b1e1493192edcf47efab
S3RG1O1994 <se.diaz60@gmail.com>
2020-03-13 20:32:07 +0000

    Answers

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,94 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+
+/**
+ * op_cha - print char.
+ * @c: char.
+ *
+ * Return: nothing;
+ */
+
+void op_cha(va_list c)
+{
+	printf("%c", va_arg(c, int));
+}
+
+/**
+ * op_int - function print integer.
+ * @i: integrer.
+ *
+ * Return: nothing.
+ */
+
+void op_int(va_list i)
+{
+	printf("%d", va_arg(i, int));
+}
+
+/**
+ * op_flo - function print floats
+ * @f: float.
+ *
+ * Return: float.
+ */
+
+void op_flo(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+
+/**
+ * op_ptr - print strings
+ * @s: pointer s.
+ *
+ * Return: integree.
+ */
+
+void op_ptr(va_list s)
+{
+	printf("%s", va_arg(s, char*));
+}
+
+/**
+ * print_all - print strings.
+ * @format: is format.
+ * Return: nothing.
+ */
+
+void print_all(const char * const format, ...)
+{
+	char *coma = "";
+	va_list data;
+	int i = 0;
+	int i2 = 0;
+
+	type array[] = {
+		{"c", op_cha},
+		{"i", op_int},
+		{"f", op_flo},
+		{"s", op_ptr},
+		{NULL, NULL}
+	};
+
+	va_start(data, format);
+
+
+	while (format != NULL && format[i] != '\0')
+	{
+		i2 = 0;
+
+		while (array[i2].arr != '\0')
+		{
+			if (array[i2].arr[0] == format[i])
+			{
+				printf("%s", coma);
+				array[i2].f(data);
+				coma = ", ";
+			}
+			i2++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(data);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,7 +4,22 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+
+typedef struct types
+{
+	char *arr;
+        void(*f)(va_list data);
+} type;
+
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
