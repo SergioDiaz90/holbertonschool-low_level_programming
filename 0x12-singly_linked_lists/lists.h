9bee26d527e717091bec73ae2d2e17ae91918764
S3RG1O1994 <se.diaz60@gmail.com>
2020-03-20 03:34:49 +0000

    new-lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef FILE_HEADER
-#define FILE_HEADER
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -26,5 +26,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void __attribute__((constructor)) before_main(void);
 
 #endif
