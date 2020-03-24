3f9eaee3fb34c0e8cb01747637e8427a31547cd0
S3RG1O1994 <se.diaz60@gmail.com>
2020-03-24 17:07:40 +0000

    Answers-2

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * print_listint_len - return number of elements in the list.
+ * @h: pointer of list.
+ * Return: lenght of list.
+ */
+size_t listint_len(const listint_t *h)
+{
+	int i = 0;
+
+	for (; h != '\0'; i++)
+	{
+		h = h->next;
+	}
+	return (i);
+}
