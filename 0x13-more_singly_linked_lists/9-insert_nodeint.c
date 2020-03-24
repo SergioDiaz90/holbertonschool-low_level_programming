e1bd9ae0a46c7960417550491b7337c4697fb486
S3RG1O1994 <se.diaz60@gmail.com>
2020-03-24 23:45:12 +0000

    Corrections

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	for (; i <= idx - 1; i++)
+	for (; i < idx - 1; i++)
 	{
 		if (tmp == NULL && idx - i > 0)
 		{
@@ -40,8 +40,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		tmp = tmp->next;
 	}
 
-		new->next = tmp->next;
-		tmp->next = new;
+	new->next = tmp->next;
+	tmp->next = new;
 
 	return (new);
 }
