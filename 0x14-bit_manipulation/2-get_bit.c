43d670a124067a2ffa4fb6a5c86feb67bb98f847
S3RG1O1994 <se.diaz60@gmail.com>
2020-03-27 03:21:30 +0000

    Corrections

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,5 +7,5 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	return((index > sizeof(n) * 8 -1) ? -1 : (int)(n >> index) & 1);
+	return ((index > sizeof(n) * 8 - 1) ? -1 : (int)(n >> index) & 1);
 }
