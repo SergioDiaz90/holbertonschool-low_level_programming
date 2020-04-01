1414ecaf3dcd8859d711af8aafc4b9707a7e3fb0
S3RG1O1994 <se.diaz60@gmail.com>
2020-04-01 00:49:21 +0000

    corrections

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	arch = open(filname, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	arch = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (arch == -1)
 		return (-1);
