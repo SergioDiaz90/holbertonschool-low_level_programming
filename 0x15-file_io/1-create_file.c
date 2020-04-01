33fa6ad0cb9cc58e5fdc4e9b34da9c2715c2fe1b
S3RG1O1994 <se.diaz60@gmail.com>
2020-04-01 00:31:44 +0000

    corrections

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content)
 	{
 		for (; text_content[cnt] != '\0'; cnt++)
-			;
+		{}
 		Wte = write(arch, text_content, cnt);
 		if (Wte == -1)
 			return (-1);
