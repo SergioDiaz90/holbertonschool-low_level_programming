00de58ed9a429c20230ab3a78fd6d3edbb98f91a
S3RG1O1994 <se.diaz60@gmail.com>
2020-04-01 00:23:14 +0000

    point-1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * create_textfile - create and write in new document.
+ * @filename: pointer an text file.
+ * @text_content: number characters.
+ * Return: document create.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int arch,Wte = NULL, cnt = 0;
+
+	if (filename == NULL)
+		return (-1);
+	arch = open(filname, O_CREAT | O_WRONLY | O-TRUNC, SIRUSR| S_IWUSR);
+
+	if (arch == -1)
+		return (-1);
+	if (text_content)
+	{
+		for(; text_content[cnt] != '\0'; cnt++)
+			;
+		Wte = write(arch, text_content, cnt);
+		if (Wte = -1)
+			return (-1);
+	}
+	close (arch);
+	return (1);
+}
