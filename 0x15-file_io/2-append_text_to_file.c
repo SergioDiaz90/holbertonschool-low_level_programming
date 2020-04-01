3f9a0c20b2b45f889d750df6cef9c17c5700a57e
S3RG1O1994 <se.diaz60@gmail.com>
2020-04-01 01:12:07 +0000

    Answers-2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * create_file - create and write in new document.
+ * @filename: pointer an text file.
+ * @text_content: number characters.
+ * Return: document create.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int arch, Wte = NULL, cnt;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
+
+	arch = open(filename, O_WRONLY | O_APPEND);
+
+	if (arch == -1)
+		return (-1);
+
+	for (cnt = 1; text_content[cnt] != '\0'; cnt++)
+	{}
+	Wte = write(arch, text_content, cnt);
+
+	if (Wte == -1)
+		return (-1);
+
+	close(arch);
+	return (1);
+}
