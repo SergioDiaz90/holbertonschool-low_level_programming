ff54ebf838b0f4bb6b7e4720bbc53eeea24b43c1
S3RG1O1994 <se.diaz60@gmail.com>
2020-04-01 00:28:55 +0000

    corrections

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,28 +1,29 @@
 #include "holberton.h"
 /**
- * create_textfile - create and write in new document.
+ * create_file - create and write in new document.
  * @filename: pointer an text file.
  * @text_content: number characters.
  * Return: document create.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int arch,Wte = NULL, cnt = 0;
+	int arch, Wte = NULL, cnt = 0;
 
 	if (filename == NULL)
 		return (-1);
-	arch = open(filname, O_CREAT | O_WRONLY | O-TRUNC, SIRUSR| S_IWUSR);
+
+	arch = open(filname, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (arch == -1)
 		return (-1);
 	if (text_content)
 	{
-		for(; text_content[cnt] != '\0'; cnt++)
+		for (; text_content[cnt] != '\0'; cnt++)
 			;
 		Wte = write(arch, text_content, cnt);
-		if (Wte = -1)
+		if (Wte == -1)
 			return (-1);
 	}
-	close (arch);
+	close(arch);
 	return (1);
 }
