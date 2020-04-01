142ae3d3b319df9f2276d5e8e75417d933c9ee33
S3RG1O1994 <se.diaz60@gmail.com>
2020-04-01 00:44:48 +0000

    corrections

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int arch, Wte = NULL, cnt = 1;
+	int arch, Wte = NULL, cnt;
 
 	if (filename == NULL)
 		return (-1);
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content)
 	{
-		for (; text_content[cnt] != '\0'; cnt++)
+		for (cnt = 1; text_content[cnt] != '\0'; cnt++)
 		{}
 		Wte = write(arch, text_content, cnt);
 		if (Wte == -1)
