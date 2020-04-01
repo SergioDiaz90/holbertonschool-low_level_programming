eeeab6eaf6a1881ca8bb8b01bff9c4fe00b104a3
S3RG1O1994 <se.diaz60@gmail.com>
2020-04-01 00:00:33 +0000

    corrections

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -3,7 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+
 #endif
