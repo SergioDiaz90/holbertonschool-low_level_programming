753e61fc3ecd633948ee39ae58fdb14ee76fba13
S3RG1O1994 <se.diaz60@gmail.com>
2020-04-22 21:26:40 +0000

    [Add]: program in C for print an string

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ *main - print string
+ *Description: print str
+ *Return: 1
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art ls useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
