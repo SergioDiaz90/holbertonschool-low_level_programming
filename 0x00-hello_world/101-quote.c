8b23ed8f8d0c68791f6866010508344382a07a01
S3RG1O1994 <se.diaz60@gmail.com>
2020-04-22 21:38:18 +0000

    [Add]: program in C for print an string

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  *main - print string
@@ -8,11 +8,6 @@
 
 int main(void)
 {
-	char str[59] = "and that piece of art ls useful\" - Dora Korpar, 2015-10-19\n";
-	int cnt;
-
-	for (cnt = 0; cnt < 59; cnt++)
-		putchar(str[cnt]);
-
+	write(1, "and that piece of art ls useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
