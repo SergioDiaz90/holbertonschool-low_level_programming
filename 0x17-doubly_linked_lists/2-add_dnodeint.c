6a1298ffc8dc9f2aba78f5a5d5a0dd9f46196984
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-02 23:01:21 -0400

    [Fix]: Delete conditional for not exists parameter n

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,16 +10,21 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
     dlistint_t *new_node = NULL, *copy = NULL;
 
-    if (!n)
-        return (NULL);
     copy = *head;
     new_node = malloc(sizeof(dlistint_t));
     if (!new_node)
         return (NULL);
     new_node->n = n;
-    new_node->next = copy;
+    if (!copy)
+    {
+        new_node->next = NULL;
+        new_node->prev = NULL;
+        *head = new_node;
+        return (new_node);
+    }
+    copy->prev = new_node;
     new_node->prev = NULL;
-    copy = new_node;
-    *head = copy;
-    return (*head);
+    *head = new_node;
+    new_node->next = copy;
+    return (new_node);
 }
