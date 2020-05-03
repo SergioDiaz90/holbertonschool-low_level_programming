f0bc7d35d79a17bc198397cd056ae316c5a539de
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-03 17:24:33 -0400

    [Fix]: Add posibilities for add node the beginning or end list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,23 +13,32 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
     copy = *h;
     
+    if (idx == 0)
+    {
+        copy = add_dnodeint(h, n);
+        return (copy);
+    }
     while (cnt_list < idx && copy->next != NULL)
     {
         copy = copy->next;
         cnt_list++;
     }
     if (idx > cnt_list)
-    {
         return (NULL);
+    if (idx == cnt_list + 1)
+    {
+        copy = add_dnodeint_end(h, n);
+        return (copy);
     }
-    copy = copy->prev;
     new_node = malloc(sizeof(dlistint_t));
     if (!new_node)
         return (NULL);
-
     new_node->n = n;
-    new_node->prev = copy;
-    new_node->next = copy->next;
+    new_node->prev = copy->prev;
+    new_node->next = copy;
+    copy = new_node;
+    copy = copy->prev;
     copy->next = new_node;
-    return (new_node);
+    copy = *h;
+    return (copy);
 }
