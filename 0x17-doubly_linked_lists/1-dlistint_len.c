807b3a372942ea12dfebf809d069f072e2f92a47
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-02 20:34:15 -0400

    [Add]: Second excersice

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * dlistint_len- function print data list
+ * @h: pointer to the list
+ *
+ * Return: lenght of the list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+    size_t num_node = 0;
+
+    if (!h)
+        return (0);
+    while (h->next)
+    {
+        h = h->next;
+        num_node += 1;
+    }
+    return (num_node);
+}
