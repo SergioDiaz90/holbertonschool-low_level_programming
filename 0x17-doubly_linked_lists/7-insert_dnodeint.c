8ad5a57e2eb7e22fcd60ee1bbbd8d9b7caabdf19
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-03 18:05:50 -0400

    [Fix]: refactor of code

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,33 +12,34 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     unsigned int cnt_list = 0;
 
     copy = *h;
-    
     if (idx == 0)
     {
         copy = add_dnodeint(h, n);
         return (copy);
     }
-    while (cnt_list < idx && copy->next != NULL)
-    {
-        copy = copy->next;
-        cnt_list++;
-    }
-    if (idx > cnt_list)
+    if (!copy || !n)
         return (NULL);
-    if (idx == cnt_list + 1)
-    {
-        copy = add_dnodeint_end(h, n);
-        return (copy);
-    }
     new_node = malloc(sizeof(dlistint_t));
     if (!new_node)
         return (NULL);
     new_node->n = n;
-    new_node->prev = copy->prev;
-    new_node->next = copy;
-    copy = new_node;
-    copy = copy->prev;
-    copy->next = new_node;
-    copy = *h;
-    return (copy);
+    while (copy)
+    {
+        if (idx == cnt_list)
+        {
+            new_node->prev = copy->prev;
+            new_node->next = copy;
+            copy = new_node;
+            copy = copy->prev;
+            copy->next = new_node;
+            copy = *h;
+            return (copy);
+        }
+        if (!copy->next && idx == cnt_list + 1)
+            return (free(new_node), add_dnodeint_end(h, n));
+        copy = copy->next;
+        cnt_list++;
+    }
+    free(new_node);
+    return (NULL);
 }
