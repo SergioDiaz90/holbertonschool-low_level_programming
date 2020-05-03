ff7c0a801816a6a8d635e4c92eee75cae7bc4fdc
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-03 00:38:18 -0400

    [Add]: fifth excercise

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_dlistint - function for free all list.
+ * @head: beginning of the list
+ *
+ * Return: nothing because is an function type void.
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+    dlistint_t *copy;
+
+    while (head)
+    {
+        copy = head;
+        head = head->next;
+        copy->prev = NULL;
+        copy->next = NULL;
+        free(copy);
+    }
+}
