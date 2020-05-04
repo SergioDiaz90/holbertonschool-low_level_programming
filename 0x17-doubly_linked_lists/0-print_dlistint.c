29813d187555465ba428d9063fefad9892da9440
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-04 04:19:52 +0000

    [Fix]: Betty style

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,25 +1,22 @@
 #include "lists.h"
 /**
- * struct dlistint_s - doubly linked list
- * @n: integer
- * @prev: points to the previous node
- * @next: points to the next node
+ * print_dlistint - print linked list
+ * @h: pointer to list
  *
- * Description: doubly linked list node structure
- * for Holberton project
+ * Return: num_node.
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t num_node = 0;
+	size_t num_node = 0;
 
-    if (!h)
-        return (0);
-    while(h)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        num_node += 1;
-    }
-    return (num_node);
+	if (!h)
+		return (0);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		num_node += 1;
+	}
+	return (num_node);
 }
