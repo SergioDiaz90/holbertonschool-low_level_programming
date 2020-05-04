f4163cd6c65004eb8f648f6838e35f66034dedd4
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-04 04:28:51 +0000

    [Fix]: Betty style

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -8,17 +8,17 @@
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int cnt_list = 0;
-    dlistint_t *copy = head;
+	unsigned int cnt_list = 0;
+	dlistint_t *copy = head;
 
-    while (cnt_list < index && copy->next != NULL)
-    {
-        copy = copy->next;
-        cnt_list++;
-    }
-    if (index > cnt_list)
-    {
-        return (NULL);
-    }
-    return (copy);
+	while (cnt_list < index && copy->next != NULL)
+	{
+		copy = copy->next;
+		cnt_list++;
+	}
+	if (index > cnt_list)
+	{
+		return (NULL);
+	}
+	return (copy);
 }
