94a636f313843dadabe0deb091471b69294de325
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-04 04:20:54 +0000

    [Fix]: Betty style

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,14 +8,14 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t num_node = 0;
+	size_t num_node = 0;
 
-    if (!h)
-        return (0);
-    while (h)
-    {
-        h = h->next;
-        num_node += 1;
-    }
-    return (num_node);
+	if (!h)
+		return (0);
+	while (h)
+	{
+		h = h->next;
+		num_node += 1;
+	}
+	return (num_node);
 }
