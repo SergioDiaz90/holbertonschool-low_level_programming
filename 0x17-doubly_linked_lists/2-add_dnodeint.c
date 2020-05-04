55d776302d0b7648e6e04e5140f3a5e180048f55
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-04 04:23:21 +0000

    [Fix]: Betty style

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,29 +2,29 @@
 /**
  * add_dnodeint - function add in first position new node.
  * @head: beginning of the list
- *
+ * @n: data for agree.
  * Return: Pointer to start beginning list.
  */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node = NULL, *copy = NULL;
+	dlistint_t *new_node = NULL, *copy = NULL;
 
-    copy = *head;
-    new_node = malloc(sizeof(dlistint_t));
-    if (!new_node)
-        return (NULL);
-    new_node->n = n;
-    if (!copy)
-    {
-        new_node->next = NULL;
-        new_node->prev = NULL;
-        *head = new_node;
-        return (new_node);
-    }
-    copy->prev = new_node;
-    new_node->prev = NULL;
-    *head = new_node;
-    new_node->next = copy;
-    return (new_node);
+	copy = *head;
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	if (!copy)
+	{
+		new_node->next = NULL;
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
+	}
+	copy->prev = new_node;
+	new_node->prev = NULL;
+	*head = new_node;
+	new_node->next = copy;
+	return (new_node);
 }
