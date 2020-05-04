3a083fd3ad660732719c7a69adc3ca9614c2de46
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-04 04:30:06 +0000

    [Fix]: Betty style

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -6,14 +6,14 @@
  */
 int sum_dlistint(dlistint_t *head)
 {
-    int sum = 0;
+	int sum = 0;
 
-    if (!head)
-        return (0);
-    while (head)
-    {
-        sum += head->n;
-        head = head->next;
-    }
-    return (sum);
+	if (!head)
+		return (0);
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
 }
