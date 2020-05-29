1e4d8ca6d461bb8a856d5c3324c7fd1a9d5540db
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-29 17:46:07 -0500

    [Add]: betty-style

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -21,11 +21,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	while (pos)
 	{
-		if(!strcmp(key, pos->key))
+		if (!strcmp(key, pos->key))
 		{
 			free(pos->value);
 			pos->value = strdup(value);
-			return(1);
+			return (1);
 		}
 		pos = pos->next;
 	}
