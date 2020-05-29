8a97748d8325677bbd8c0dec3540b20fa13ed5ec
S3RG1O1994 <se.diaz60@gmail.com>
2020-05-29 17:51:01 -0500

    [Add]: fixed conditionals

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,6 +12,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *tmp = NULL;
 
+	if (!ht || !key)
+		return (NULL);
+
 	index = key_index((unsigned char *)key, ht->size);
 	tmp = ht->array[index];
 
