f2659ddd48d6cc3ff167c305f056c8137c35f50a
S3RG1O1994 <se.diaz60@gmail.com>
2020-07-03 23:55:35 -0500

    [new]: new validations

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,17 +7,17 @@
 def island_perimeter(grid):
     '''Function for find the perimeter of an island'''
 
-    add_w = 0
-    add_h = 0
+    perimeter = 0
 
-    for cols in grid:
-        for cnt2 in range(len(cols)):
-            if cnt2 < len(cols):
-                if cols[cnt2] == 1 and cols[cnt2 - 1] == 1:
-                    if cols[cnt2 - 2] == 0:
-                        add_w += 2
-                    else:
-                        add_w += 1
-                if cols[cnt2] == 1 and cols[cnt2 - 1] == 0:
-                    add_h += 1
-    return (add_h + add_w) * 2
+    for cols in range(len(grid)):
+        for cnt2 in range(len(grid[cols])):
+            if grid[cols][cnt2] == 1:
+                if cols - 1 == -1 or grid[cols - 1][cnt2] == 0:
+                    perimeter += 1
+                if cnt2 == len(grid[cols]) - 1 or grid[cols][cnt2 + 1] == 0:
+                    perimeter += 1
+                if cols == len(grid) - 1 or grid[cols + 1][cnt2] == 0:
+                    perimeter += 1
+                if cnt2 - 1 == -1 or grid[cols][cnt2 - 1] == 0:
+                    perimeter += 1
+    return perimeter
