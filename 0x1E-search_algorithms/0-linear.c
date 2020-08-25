10a4b5a13768f0787b9673f9c827a75d452fd8d2
S3RG1O1994 <se.diaz60@gmail.com>
2020-08-25 17:11:42 -0500

    [Add]: New function for search value with the method binary [Add]: comments about the function of linear_search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,32 @@
+#include "search_algos.h"
+/**
+ * linear_search - Function for search an value in sort array.
+ * @array: Pointer first item of array.
+ * @size: Size of array.
+ * @value: Value to searched in array.
+ * Return: cnt in case of success or -1 if not found value.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+    int *tmp = NULL;
+    size_t cnt = 0;
+
+    if (!array)
+        return (-1);
+    
+    tmp = array;
+
+    while (cnt < size && tmp[cnt] != value)
+    {
+        printf("Value checked array [%d] = [%d]\n", (int)cnt, tmp[cnt]);
+        cnt += 1;
+    }
+
+    if (tmp[cnt] == value)
+    {
+        printf("Value checked array [%d] = [%d]\n", (int)cnt, tmp[cnt]);
+        return (cnt);
+    }
+    else
+        return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,7 +5,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
 /*prototypes*/
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 #endif // !"search_algos.h"
