74c0c73299541a812c9189d4166554f86eebd0b0
S3RG1O1994 <se.diaz60@gmail.com>
2020-08-25 12:27:14 -0500

    [Add]: init project and first program of function search linear

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef search_algos
+#define search_algos
+
+/*libraries*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*prototypes*/
+int linear_search(int *array, size_t size, int value);
+#endif // !"search_algos.h"
